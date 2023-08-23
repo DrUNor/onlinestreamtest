46f824388278ce14048b4a19348465e3a26ae88c
DrUNor <142967588+DrUNor@users.noreply.github.com>
2023-08-23 10:16:52 +0200

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -1,104 +1,49 @@
-const video = document.getElementById('video');
-const videoContainer = document.getElementById('video-container');
-const muteButton = document.getElementById('mute-button');
-const unmuteButton = document.getElementById('unmute-button');
-const fullscreenButton = document.getElementById('fullscreen-button');
+const openingContainer = document.getElementById('opening-container');
+const viewerButton = document.getElementById('viewer-button');
+const hostButton = document.getElementById('host-button');
+const streamingContainer = document.getElementById('streaming-container');
+const viewerContainer = document.getElementById('viewer-container');
+const chooseViewerContainer = document.getElementById('choose-viewer');
+const joinViewerButton = document.getElementById('join-viewer-button');
 const startStreamButton = document.getElementById('start-stream-button');
 const stopStreamButton = document.getElementById('stop-stream-button');
-const recordButton = document.getElementById('record-button');
-const stopRecordButton = document.getElementById('stop-record-button');
-const viewerButton = document.getElementById('viewer-button');
+const hostVideo = document.getElementById('host-video');
 const viewerVideo = document.getElementById('viewer-video');
-const socket = io();
 
-let mediaRecorder;
-let recordedChunks = [];
-
-muteButton.addEventListener('click', () => {
-  video.muted = true;
-});
-
-unmuteButton.addEventListener('click', () => {
-  video.muted = false;
-});
-
-fullscreenButton.addEventListener('click', () => {
-  if (videoContainer.requestFullscreen) {
-    videoContainer.requestFullscreen();
-  }
-});
-
-startStreamButton.addEventListener('click', async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-  video.srcObject = stream;
-  mediaRecorder = new MediaRecorder(stream);
-  mediaRecorder.ondataavailable = (event) => {
-    if (event.data.size > 0) {
-      recordedChunks.push(event.data);
-    }
-  };
-  mediaRecorder.start();
-  socket.emit('startStream');
-});
-
-stopStreamButton.addEventListener('click', () => {
-  video.srcObject = null;
-  mediaRecorder.stop();
-  socket.emit('stopStream');
-});
-
-recordButton.addEventListener('click', () => {
-  recordedChunks = [];
-  mediaRecorder.start();
-});
-
-stopRecordButton.addEventListener('click', () => {
-  mediaRecorder.stop();
-});
-
-socket.on('stream', (data) => {
-  const blob = new Blob([data], { type: 'video/webm' });
-  const url = URL.createObjectURL(blob);
-  video.src = url;
-});
-
-socket.on('startStream', () => {
-  console.log('Stream started on another client.');
-});
-
-socket.on('stopStream', () => {
-  console.log('Stream stopped on another client.');
-});
-
-
+// Handle viewer button click
 viewerButton.addEventListener('click', () => {
-  viewerButton.disabled = false; // Disable the button to prevent multiple clicks
-  // Send a request to the server to join as a viewer
-  socket.emit('joinViewer');
-  // Hide the "Join as Viewer" button
-  document.getElementById('choose-viewer').style.display = 'none';
+  openingContainer.style.display = 'none';
+  viewerContainer.style.display = 'block';
+  chooseViewerContainer.style.display = 'block';
 });
 
-socket.on('streamToViewers', (data) => {
-  const blob = new Blob([data], { type: 'video/webm' });
-  const url = URL.createObjectURL(blob);
-  viewerVideo.src = url;
+// Handle host button click
+hostButton.addEventListener('click', () => {
+  openingContainer.style.display = 'none';
+  streamingContainer.style.display = 'block';
 });
 
-socket.on('startStream', () => {
-  console.log('Stream started on another client.');
-  // Automatically trigger the video play when the broadcast starts
-  viewerVideo.play();
+// Handle join viewer button click
+joinViewerButton.addEventListener('click', () => {
+  viewerContainer.style.display = 'none';
+  chooseViewerContainer.style.display = 'none';
+  viewerVideo.style.display = 'block';
+  // Call the function to join as a viewer
+  joinViewer();
 });
 
-socket.on('stopStream', () => {
-  console.log('Stream stopped on another client.');
-  // Stop the video playback when the broadcast stops
-  viewerVideo.pause();
+// Handle start stream button click
+startStreamButton.addEventListener('click', () => {
+  startStreamButton.disabled = true;
+  stopStreamButton.disabled = false;
+  // Call the function to start the stream as a host
+  startStream();
 });
 
-
-
-
-
-
+// Handle stop stream button click
+stopStreamButton.addEventListener('click', () => {
+  startStreamButton.disabled = false;
+  stopStreamButton.disabled = true;
+  // Call the function to stop the stream as a host
+  stopStream();
+});
--- server.js
+++ server.js
@@ -1,37 +1,47 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-
-  socket.on('stream', (data) => {
-    socket.broadcast.emit('stream', data);
-  });
-});
+const io = socketIO(server);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   // Handle joining as a viewer
   socket.on('joinViewer', () => {
-    // Join the "viewers" room
     socket.join('viewers');
   });
 
+  // Handle starting the stream as a host
+  socket.on('startStream', () => {
+    console.log('Stream started');
+    socket.to('viewers').emit('startStream');
+  });
+
+  // Handle stopping the stream as a host
+  socket.on('stopStream', () => {
+    console.log('Stream stopped');
+    socket.to('viewers').emit('stopStream');
+  });
+
+  // Handle sending video stream data to viewers
+  socket.on('stream', (data) => {
+    socket.to('viewers').emit('stream', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
