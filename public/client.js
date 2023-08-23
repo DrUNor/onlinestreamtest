02b7a61493abc138e1e8cca66566500c93eba5b5
DrUNor <142967588+DrUNor@users.noreply.github.com>
2023-08-23 10:57:22 +0200

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -4,24 +4,27 @@ const hostButton = document.getElementById('host-button');
 const streamingContainer = document.getElementById('streaming-container');
 const viewerContainer = document.getElementById('viewer-container');
 const chooseViewerContainer = document.getElementById('choose-viewer');
-const joinViewerButton = document.getElementById('join-viewer-button');
 const startStreamButton = document.getElementById('start-stream-button');
 const stopStreamButton = document.getElementById('stop-stream-button');
-const hostVideo = document.getElementById('host-video');
-const viewerVideo = document.getElementById('viewer-video');
 const muteButton = document.getElementById('mute-button');
 const unmuteButton = document.getElementById('unmute-button');
-
+const hostVideo = document.getElementById('host-video');
+const waitingMessage = document.getElementById('waiting-message'); // Add this line
 
 let mediaStream = null;
 let isMuted = false;
+
 
 // Handle viewer button click
 viewerButton.addEventListener('click', () => {
   openingContainer.style.display = 'none';
   viewerContainer.style.display = 'block';
-  chooseViewerContainer.style.display = 'block';
+  waitingMessage.style.display = 'block'; // Show waiting message
+  // Call the function to connect to the host's stream
+  connectToHostStream();
 });
+
+
 
 // Handle host button click
 hostButton.addEventListener('click', () => {
@@ -29,41 +32,12 @@ hostButton.addEventListener('click', () => {
   streamingContainer.style.display = 'block';
 });
 
-// Handle join viewer button click
-joinViewerButton.addEventListener('click', () => {
-  viewerContainer.style.display = 'none';
-  chooseViewerContainer.style.display = 'none';
-  viewerVideo.style.display = 'block';
-  // Call the function to join as a viewer
-  joinViewer();
-});
-
-function joinViewer() {
-  try {
-    mediaStream = new MediaStream();
-    const viewerStream = viewerVideo.captureStream();
-    mediaStream.addTrack(viewerStream.getAudioTracks()[0]);
-    // Call the function to connect to the host's stream
-    connectToHostStream();
-  } catch (error) {
-    console.error('Error joining as viewer:', error);
-  }
-}
-
-function connectToHostStream() {
-  const viewerSocket = io();
-
-  viewerSocket.on('stream', (data) => {
-    const blob = new Blob([data], { type: 'video/webm' });
-    const url = URL.createObjectURL(blob);
-    viewerVideo.src = url;
-  });
-}
 
 // Handle start stream button click
 startStreamButton.addEventListener('click', () => {
   startStreamButton.disabled = true;
   stopStreamButton.disabled = false;
+  muteButton.style.display = 'inline-block';
   // Call the function to start the stream as a host
   startStream();
 });
@@ -92,11 +66,14 @@ unmuteButton.addEventListener('click', () => {
   muteButton.style.display = 'inline-block';
 });
 
+// Start stream function
 async function startStream() {
   try {
     mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     hostVideo.srcObject = mediaStream;
     hostVideo.play();
+    // Emit 'startStream' event to the server
+    socket.emit('startStream');
   } catch (error) {
     console.error('Error starting stream:', error);
   }
@@ -108,5 +85,40 @@ function stopStream() {
     mediaStream.getTracks().forEach((track) => track.stop());
     mediaStream = null;
     hostVideo.srcObject = null;
+    // Emit 'stopStream' event to the server
+    socket.emit('stopStream');
   }
+}
+
+// Create Socket.IO connection
+const socket = io();
+
+// Join as viewer function
+function joinViewer() {
+  try {
+    mediaStream = new MediaStream();
+    const viewerStream = viewerVideo.captureStream();
+    mediaStream.addTrack(viewerStream.getAudioTracks()[0]);
+    // Emit 'joinViewer' event to the server
+    socket.emit('joinViewer');
+    // Call the function to connect to the host's stream
+    connectToHostStream();
+  } catch (error) {
+    console.error('Error joining as viewer:', error);
+  }
+}
+
+// Connect to host's stream function
+function connectToHostStream() {
+  const viewerSocket = io();
+  
+  viewerSocket.on('startStream', () => {
+    waitingMessage.style.display = 'none'; // Hide waiting message when stream starts
+  });
+  
+  viewerSocket.on('stream', (data) => {
+    const blob = new Blob([data], { type: 'video/webm' });
+    const url = URL.createObjectURL(blob);
+    viewerVideo.src = url;
+  });
 }
